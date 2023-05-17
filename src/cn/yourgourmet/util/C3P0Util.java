39a311f3216cd9cc1ebb3369b8d4a66fd2d12fd8
OneBumps <3259851304@qq.com>
2023-05-17 20:43:01 +0800

    测试数据库连接池连接

--- src/cn/yourgourmet/util/C3P0Util.java
+++ src/cn/yourgourmet/util/C3P0Util.java
@@ -22,31 +22,4 @@ public class C3P0Util {
         }
         return null;
     }
-
-    public static void release(Connection conn, Statement stmt, ResultSet rs) {
-        if (rs != null) {
-            try {
-                rs.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            rs = null;
-        }
-        if (stmt != null) {
-            try {
-                stmt.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            stmt = null;
-        }
-        if (conn != null) {
-            try {
-                conn.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            conn = null;
-        }
-    }
 }
