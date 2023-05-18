b00598abde4113e4183705f821b18d8c99fce437
OneBumps <3259851304@qq.com>
2023-05-18 18:21:45 +0800

    修改space的js文件

--- web/js/space/main.js
+++ web/js/space/main.js
@@ -1,7 +1,18 @@
+/**
+ * 用于space页面的js
+ * 依赖于/js/util/ajax.js
+ */
+
 document.write("<script src='/js/util/ajax.js'></script>");
 
 function getPage_Summary() {
-
+    sendRequest("/space/summary", "", "POST", function () {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                document.getElementById("content").innerHTML = httpRequest.responseText;
+            }
+        }
+    });
 }
 
 document.getElementById("nav-summary").addEventListener("click", getPage_Summary);
