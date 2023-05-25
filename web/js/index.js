d97bce462afd7042359f8c26c5f027296380c045
SeviLuo <2832659585@qq.com>
2023-05-25 15:17:35 +0800

    写了nav

--- web/js/index.js
+++ web/js/index.js
@@ -1,6 +1,7 @@
 // 设置基准浮标和颜色
 var index = 0;
-var colors = ["#ff8c00", "#7a6ada","#b1191a","#6ada84","#6a95da","#1564AF"];
+var colors = ["#ff8c00", "#7a6ada", "#b1191a", "#6ada84", "#6a95da", "#1564AF"];
+var box_name = ["#collection", "#my-menu"];
 var color = colors[0];
 
 function toggleAnimation(now_index, now_color) {
@@ -8,6 +9,9 @@ function toggleAnimation(now_index, now_color) {
     $("#nav-tracker").css("left", now_index * 150 + 20 + "px");
     // 设置nav-tracker的背景颜色为当前点击的a标签的颜色
     $("#nav-tracker").css("background-color", now_color);
+    // 设置a标签变色
+    $(".navitems a").css("color", now_color);
+    $($(".navitems a").parent()[now_index]).siblings().children().css("color", "#000");
 }
 
 $(".navitems li a").click(function () {
@@ -17,18 +21,27 @@ $(".navitems li a").click(function () {
     color = colors[index];
     // 设置切换动画
     toggleAnimation(index, color);
-    
 });
 
-$(".navitems li a").hover(function () {
-    // 获取当前点击的a标签所在的li的索引
-    var hover_index = $(this).parent().index();
-    // 获取当前点击的a标签所在的li的颜色
-    var hover_color = colors[hover_index];
-    // 设置切换动画
-    toggleAnimation(hover_index, hover_color);
-});
+$(".navitems li a").hover(
+    function () {
+        // 获取当前点击的a标签所在的li的索引
+        var hover_index = $(this).parent().index();
+        // 获取当前点击的a标签所在的li的颜色
+        var hover_color = colors[hover_index];
+        // 设置切换动画
+        toggleAnimation(hover_index, hover_color);
+    },
+    function () {
+        toggleAnimation(index, color);
+    });
 
 $(".navitems li a").mouseleave(function () {
     toggleAnimation(index, color);
 });
+
+$(document).ready(function () {
+    // 设置切换动画
+    toggleAnimation(index, color);
+}
+);
