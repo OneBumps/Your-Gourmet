c0f48ccc831f25e66da9d823e9ebb5172d0cf66d
OneBumps <3259851304@qq.com>
2023-05-26 20:27:49 +0800

    修改tracker

--- src/main/web/js/util/ajax.js
+++ src/main/web/js/util/ajax.js
@@ -8,12 +8,14 @@ function createXHR() {
 }
 
 
-//发送客户端的请求，该方法有4个参数，
-//参数url设置要连接的URL，建立到服务器的连接并执行
-//参数params为从Web表单中获取需要发送的数据集合
-//参数method取值为POST或GET
-//参数handler为指定的响应函数（服务器在完成后要运行的回调函数）
-function sendRequest(url, params, method, handler) {
+/**
+ * 使用XMLHttpRequest发送客户端的请求
+ * @param {String} url 设置要连接的URL，建立到服务器的连接并执行
+ * @param params 从Web表单中获取需要发送的数据集合
+ * @param method 设置请求的类型，GET或POST，默认为POST
+ * @param {Function} handler 指定的响应函数（服务器在完成后要运行的回调函数）
+ */
+function sendRequest(url, params, method = "POST", handler) {
     createXHR();
     if (!httpRequest) return false;
     httpRequest.onreadystatechange = handler;
