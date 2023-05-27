f5ae6bd673a6381935de0b10c0260ce1cd1b13c6
OneBumps <3259851304@qq.com>
2023-05-27 23:10:28 +0800

    完成对tracker的修改

--- src/main/web/js/public/util/colorful-tracker.js
+++ src/main/web/js/public/util/colorful-tracker.js
@@ -1,7 +1,13 @@
 /**
  * 拥有彩色变换的nav和tracker
- * @param {String} clickItems 点击项(使用CSS选择器)
- * @param {String} trackerItem tracker项(使用CSS选择器)
+ * 结构如下：
+ * <ul>
+ *   <li><a>1</a></li>
+ *   <li><a>2</a></li>
+ *   <li><a>3</a></li>
+ * </ul>
+ * <div "tracker"></div>
+ * @param ulTagName 导航栏的ul(使用CSS选择器)
  * @param {String[]} colors 导航栏颜色数组，之后会通过colors的length判断导航栏条目数量
  * @param pageIndex 目前导航条所在的索引
  * @param leftOffset 左偏移量设置
@@ -9,12 +15,10 @@
  * @param {String[]} boxes 区块内容的名称数组(使用CSS选择器)
  * @author OneBumps
  */
-const getColorfulTracker = function (clickItems, trackerItem, colors, pageIndex = 0, leftOffset = 0, changeBg = false, changeBlock = []) {
+const getColorfulTracker = function (ulTagName, colors, pageIndex = 0, leftOffset = 0, changeBg = false, changeBlock = []) {
     // 初始化变量
     let index = pageIndex;
-    const trackerElement = document.querySelector(trackerItem);
-    const clickElements = document.querySelectorAll(clickItems);
-    const liElements = document.querySelector(clickItems).parentElement.parentElement.children;
+    const ulElement = document.querySelector(ulTagName);
     // 工作函数
     function changeBk() {
         for (let i = 0; i < changeBlock.length; i++) {
@@ -24,20 +28,19 @@ const getColorfulTracker = function (clickItems, trackerItem, colors, pageIndex 
     }
     function toggleStatus(nowIndex, nowColor) {
         // 设置偏移
-        trackerElement.style.left = leftOffset + nowIndex * (+clickElements[0].parentElement.offsetWidth) + "px";
+        ulElement.nextSibling.nextSibling.style.left = leftOffset + nowIndex * (+ulElement.children[0].offsetWidth) + "px";
         // 设置变色
-        trackerElement.style.backgroundColor = nowColor;
+        ulElement.nextSibling.nextSibling.style.backgroundColor = nowColor;
         // 设置点击项变色
-        for (let i = 0; i < liElements.length; i++) {
-            liElements[i].children[0].style.color = "#000";
+        for (let i = 0; i < ulElement.children.length; i++) {
+            ulElement.children[i].children[0].style.color = "#000";
         }
-        liElements[nowIndex].children[0].style.color = nowColor;
+        ulElement.children[nowIndex].children[0].style.color = nowColor;
     }
     function clickNav(e) {
-        e.stopPropagation();
         if (e.target.tagName !== "A") return;
         // 更新索引为当前点击的标签所在的li的索引
-        index = Array.from(liElements).indexOf(e.target.parentElement);
+        index = Array.from(ulElement.children).indexOf(e.target.parentElement);
         toggleStatus(index, colors[index]);
         // 设置全局的背景色
         if (changeBg) {
@@ -47,18 +50,18 @@ const getColorfulTracker = function (clickItems, trackerItem, colors, pageIndex 
         if (changeBlock.length > 0) changeBk();
     }
     function enterNav() {
-        let tempIndex = Array.from(liElements).indexOf(this.parentElement);
+        let tempIndex = Array.from(ulElement.children).indexOf(this.parentElement);
         toggleStatus(tempIndex, colors[tempIndex]);
     }
     function leaveNav() {
         toggleStatus(index, colors[index]);
     }
     // 设置事件
-    clickElements.forEach(function (element) {
-        element.addEventListener("mouseenter", enterNav);
+    Array.from(ulElement.children).forEach(function (element) {
+        element.children[0].addEventListener("mouseenter", enterNav);
     });
-    clickElements[0].parentElement.parentElement.addEventListener("click", clickNav);
-    clickElements[0].parentElement.parentElement.addEventListener("mouseleave", leaveNav);
+    ulElement.addEventListener("click", clickNav);
+    ulElement.addEventListener("mouseleave", leaveNav);
     // 初始化界面
     toggleStatus(index, colors[index]);
     // 设置全局的背景色
