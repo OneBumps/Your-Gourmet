d5efc2e413980081e402fd53d03c0ad6e70d6d27
SeviLuo <2832659585@qq.com>
2023-06-06 23:07:50 +0800

    完成轮播图

--- src/main/web/js/public/header.js
+++ src/main/web/js/public/header.js
@@ -5,3 +5,4 @@ script.onload = function () {
     getColorfulTracker(".navitems ul", ["#ff8c00", "#7a6ada", "#b1191a", "#6ada84", "#6a95da", "#1564AF"], 0, 20);
 };
 document.body.appendChild(script);
+
