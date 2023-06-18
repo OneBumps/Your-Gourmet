1aa1153455698cb7aee8e2de0a9544a49823c997
OneBumps <3259851304@qq.com>
2023-06-18 15:06:17 +0800

    完善后端程序

--- src/main/java/cn/yourgourmet/mapper/CuisineMapper.java
+++ src/main/java/cn/yourgourmet/mapper/CuisineMapper.java
@@ -1,7 +1,9 @@
 package cn.yourgourmet.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 
 public interface CuisineMapper {
+    String selectCuisineNameByRecipeId(@Param("cuisineId") Integer cuisineId);
 }
--- src/main/java/cn/yourgourmet/mapper/UsesMapper.java
+++ src/main/java/cn/yourgourmet/mapper/UsesMapper.java
@@ -1,7 +1,15 @@
 package cn.yourgourmet.mapper;
 
+import cn.yourgourmet.entity.Uses;
+import org.apache.ibatis.annotations.MapKey;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 
 public interface UsesMapper {
+    @MapKey("id")
+    List<Map<String, Object>> selectUsesByRecipeId(@Param("recipeId") String id);
 }
